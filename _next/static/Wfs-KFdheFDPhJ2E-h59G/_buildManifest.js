5837e73729f809ef921a11c6ea187b46cbd11403
mmmmatsuuuu <mmmmatsuuuu@users.noreply.github.com>
2022-03-10 14:33:35 +0000

    deploy: 02b1cbbe21c61fdca59b9aa2e1b4dccd7489cc49

---
+++ _next/static/Wfs-KFdheFDPhJ2E-h59G/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e1f430d3f896e5ff.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
